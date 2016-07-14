77071f09027bd2b1801802551400630450f1c8fd
roberto <rogonzalez@interware.com.mx>
2016-07-14 09:30:22 -0500

    Tabla dinámica

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import { render } from 'react-dom';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
@@ -6,17 +6,32 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
 
+const hola = [
+    {
+        id: 1,
+        name: 'Zami',
+        status: 'Chido'
+    },
+    {
+        id: 2,
+        name: 'Zami2',
+        status: 'Chido-One'
+    }
+]
+
 class Main extends React.Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <TableExampleSimple />
+        <TableExampleSimple listaCosas={hola}/>
       </MuiThemeProvider>
     );
   }
 }
 
-const TableExampleSimple = () => (
+
+
+const TableExampleSimple = ({listaCosas}) => (
   <Table>
     <TableHeader>
       <TableRow>
@@ -26,29 +41,25 @@ const TableExampleSimple = () => (
       </TableRow>
     </TableHeader>
     <TableBody>
-      <TableRow>
-        <TableRowColumn>1</TableRowColumn>
-        <TableRowColumn>John Smith</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>2</TableRowColumn>
-        <TableRowColumn>Randal White</TableRowColumn>
-        <TableRowColumn>Unemployed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>3</TableRowColumn>
-        <TableRowColumn>Stephanie Sanders</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>4</TableRowColumn>
-        <TableRowColumn>Steve Brown</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
+        {listaCosas.map((element, index) => {
+            return (
+                <TableRow key={index}>
+                    <TableRowColumn>{element.id}</TableRowColumn>
+                    <TableRowColumn>{element.name}</TableRowColumn>
+                    <TableRowColumn>{element.status}</TableRowColumn>
+                </TableRow>
+            )
+        })}
+      
+      
     </TableBody>
   </Table>
 );
+
+TableExampleSimple.propTypes = {
+	listaCosas: PropTypes.array.isRequired
+};
+
 
 render(<Main />,
 	document.getElementById('app')
