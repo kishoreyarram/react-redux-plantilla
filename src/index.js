5ba0c80d77382b88df92a9a5411636623a549131
roberto <rogonzalez@interware.com.mx>
2016-07-18 20:14:16 -0500

    Immutable example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-alert('hola');
+import Immutable from 'immutable';
+
+let pregunta = new Immutable.Record({
+    idPregunta: 0,
+    texto: undefined
+});
+
+let pregunta1 = new pregunta();
+
+
+pregunta1 = pregunta1.set('texto', 'Quien?');
+
+
+alert(pregunta1.get('texto'));
+
+let map = new Immutable.Map();
+
+map = map.set('vaca', {a:'b', c:'d'});
+
+alert(map.get('vaca').a);
+
+
+let lista = new Immutable.List();
+
+lista.push({a:'b', c:'d'});
+
+alert(lista.size);
